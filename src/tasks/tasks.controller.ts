0f0d1714114671197b47fd31f1fffa987ed2ed95
Brian Rudolph <brian.rudolph303@gmail.com>
2021-09-14 08:58:13 -0600

    feat: Added resolvers for creating and accessing dummy tasks in local storage

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,10 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Task } from './tasks.model';
 import { TasksService } from './tasks.service';
 
+// Can change this to be a resolver for GraphQL
 @Controller('tasks')
 export class TasksController {
     constructor(private tasksService: TasksService){}
 
+    @Get()
+    getAllTasks(): readonly Task[] {
+        return this.tasksService.getAllTasks();
+    }
 
-    
+    @Post()
+    createTask(@Body("title") title: string, @Body("description") description: string ): Task {
+        return this.tasksService.createTask(title,description);
+    }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './tasks.model';
+import { v4 as uuid } from "uuid";
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    // Temp before Database setup
+    private tasks: Task[] = [];
+
+    getAllTasks(): readonly Task[] {
+        return this.tasks;
+    }
+
+    createTask(title: string, description: string): Task {
+       const createdTask: Task = {
+            id: uuid(),
+            title,
+            description,
+            status: TaskStatus.OPEN,
+        }
+
+        this.tasks.push(createdTask);
+        return createdTask;
+    }
+}
