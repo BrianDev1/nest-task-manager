c3714d83f48006c6f441ebd3a5ede8d6a1d5f91c
Brian Rudolph <brian.rudolph303@gmail.com>
2021-09-15 16:23:35 -0600

    Going live

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,13 @@
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
 
 async function bootstrap() {
-  const logger = new Logger();
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3002);
-  logger.log(`Application listening on port ${3002}`);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
