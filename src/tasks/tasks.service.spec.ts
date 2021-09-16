64d958c12aefa7f73714de3d35c1551e2d6a5c79
Brian Rudolph <brian.rudolph303@gmail.com>
2021-09-16 09:30:44 -0600

    fix: Added tets

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,12 +1,28 @@
+import { Injectable } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from '../auth/auth.module';
+import { TasksReposity } from './tasks.repository';
+import { TaskStatus } from './tasks.schema';
 import { TasksService } from './tasks.service';
 
-describe('TasksService', () => {
-  let service: TasksService;
+/* For Testing purposes */
+const mockService = () => ({
+  getTasks: jest.fn(),
+});
 
+const mockUser = {
+  username: 'Test',
+  id: 'someId',
+  password: 'something',
+  tasks: [],
+};
+
+describe('TasksService', () => {
+  let service;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TasksService],
+      providers: [{ provide: TasksService, useFactory: mockService }],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
@@ -14,5 +30,17 @@ describe('TasksService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getTasks', () => {
+    it('Get All tasks from the tasks repository', async () => {
+      service.getTasks.mockResolvedValue('someVal');
+      const tasks = await service.getTasks(
+        { status: TaskStatus.OPEN, search: '' },
+        mockUser,
+      );
+      console.log(tasks);
+      expect(tasks).toEqual('someVal');
+    });
   });
 });
